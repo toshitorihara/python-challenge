2a1178cd02efbe17368edba2b7f21f004db1476a
Toshi Torihara <phenomenon1223@gmail.com>
2021-05-24 09:12:07 -0700

    Update main.py

--- PyBank/main.py
+++ PyBank/main.py
@@ -1,5 +1,6 @@
 import os
 import csv
+csv_file = os.path.join("Resources", "bank.csv")
 with open('bank.csv', newline='') as csvfile:
 plreader = csv.reader(csvfile, delimiter=' ')
 for row in plreader:
