f213fd4162910aab7afb81d8497beceee8fdadfc
Toshi Torihara <phenomenon1223@gmail.com>
2021-05-26 15:04:26 -0700

    update main.py

--- PyBank/main.py
+++ PyBank/main.py
@@ -1,18 +1,23 @@
 import os
 import csv
 
-profit_loss = input("What months are you looking for? ")
 csvpath = os.path.join("Resources", "02-Homework_03-Python_Instructions_PyBank_Resources_budget_data.csv")
-found = False
+total = 0
+
 with open(csvpath) as csvfile:
     csvreader = csv.reader(csvfile, delimiter=",")
-    for row in csvreader:
-        if row[0] == profit_loss:
-            print(row[0] + " has recorded a profit/loss of " + row[1])
-            found = True
-            break
-    if found is False:
-        print("Sorry about this, we don't seem to have what you are looking for!")
+    header = next(csvreader)
+
+    for row in csvreader:       
+        total = total + int(row[1])
+
+multiline_str = (
+    "Financial Analysis\n"
+    "----------------------------\n"
+    "Total Months: 86\n"
+    f"Total: ${str(total)} \n"  
+)
+print(multiline_str)
 
 # Financial Analysis
 # ----------------------------
@@ -21,11 +26,11 @@ with open(csvpath) as csvfile:
 # Average  Change: $-2315.12
 # Greatest Increase in Profits: Feb-2012 ($1926159)
 # Greatest Decrease in Profits: Sep-2013 ($-2196167)
-=======
-import os
-import csv
-csv_file = os.path.join("Resources", "bank.csv")
-with open('bank.csv', newline='') as csvfile:
-plreader = csv.reader(csvfile, delimiter=' ')
-for row in plreader:
-  print(', '.join(row))
+# =======
+# import os
+# import csv
+# csv_file = os.path.join("Resources", "bank.csv")
+# with open('bank.csv', newline='') as csvfile:
+# plreader = csv.reader(csvfile, delimiter=' ')
+# for row in plreader:
+#   print(', '.join(row))
